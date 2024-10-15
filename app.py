d31f707f0a178b15193679cf2cda98c06f8fded1
Ефимова Юлия <uefimova81@gmail.com>
2024-10-15 13:57:14 +0700

    Слэши в адресе

--- app.py
+++ app.py
@@ -253,3 +253,11 @@ def ecstra():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.route('/lab2/a')
+def a():
+    return 'без слэша'
+
+@app.route('/lab2/a/')
+def a2():
+    return 'со слэшем'
