355ac5aa848a582f3cd9822f2f1aea2a440e8ef8
Ефимова Юлия <uefimova81@gmail.com>
2024-11-10 17:53:25 +0700

    список пользователей

--- lab4.py
+++ lab4.py
@@ -142,6 +142,13 @@ def tree():
 
     return redirect('/lab4/tree')
 
+users = [
+    {'login': 'alex', 'password': '123'},
+    {'login': 'bob', 'password': '555'},
+    {'login': 'alik', 'password': '666'},
+    {'login': 'egor', 'password': '777'},
+]
+
 @lab4.route('/lab4/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
@@ -150,8 +157,9 @@ def login():
     login = request.form.get('login')
     password = request.form.get('password')
 
-    if login == 'alex' and password == '123':
-        return render_template('/lab4/login.html', login=login, authorized=True)
+    for user in users:
+        if login == user['login'] and password == user['password']:
+            return render_template('/lab4/login.html', login=login, authorized=True)
     
     error = 'Неверные логин и/или пароль'
     return render_template('lab4/login.html', error = error, authorized=False)
