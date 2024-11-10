91e86b0e0d4e483deeb88835a22e9936088025c3
Ефимова Юлия <uefimova81@gmail.com>
2024-11-10 16:47:37 +0700

    Счетчик деревьев и валидация

--- lab4.py
+++ lab4.py
@@ -122,3 +122,22 @@ def pow():
     except ValueError:
         return render_template('lab4/pow.html', error='Пожалуйста, введите корректные числа!')
 
+tree_count = 0
+
+
+@lab4.route('/lab4/tree', methods=['GET', 'POST'])
+def tree():
+    global tree_count
+    if request.method == 'GET':
+        return render_template('lab4/tree.html', tree_count=tree_count)
+
+    operation = request.form.get('operation')
+
+    if operation == 'cut':
+        if tree_count > 0:
+            tree_count -= 1
+    elif operation == 'plant':
+        if tree_count < 10:
+            tree_count += 1
+
+    return redirect('/lab4/tree')
