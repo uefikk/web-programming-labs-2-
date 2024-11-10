394493308f01a4a76a8f0829952804319e31573b
Ефимова Юлия <uefimova81@gmail.com>
2024-11-10 16:27:09 +0700

    арифметика

--- lab4.py
+++ lab4.py
@@ -32,4 +32,93 @@ def div():
     
     except ValueError:
         return render_template('lab4/div.html', error='Пожалуйста, введите корректные числа!')
+    
+@lab4.route('/lab4/sum-form')
+def sum_form():
+    return render_template('lab4/sum-form.html')
+
+
+@lab4.route('/lab4/sum', methods=['POST'])
+def sum():
+    x1 = request.form.get('x1', 0)
+    x2 = request.form.get('x2', 0)
+    
+    try:
+        x1 = int(x1)
+        x2 = int(x2)
+        result = x1 + x2
+        return render_template('lab4/sum.html', x1=x1, x2=x2, result=result)
+    
+    except ValueError:
+        return render_template('lab4/sum.html', error='Пожалуйста, введите корректные числа!')
+
+
+@lab4.route('/lab4/mul-form')
+def mul_form():
+    return render_template('lab4/mul-form.html')
+
+
+@lab4.route('/lab4/mul', methods=['POST'])
+def mul():
+    x1 = request.form.get('x1', 1)
+    x2 = request.form.get('x2', 1)
+    
+    try:
+        x1 = int(x1)
+        x2 = int(x2)
+        result = x1 * x2
+        return render_template('lab4/mul.html', x1=x1, x2=x2, result=result)
+    
+    except ValueError:
+        return render_template('lab4/mul.html', error='Пожалуйста, введите корректные числа!')
+
+
+@lab4.route('/lab4/sub-form')
+def sub_form():
+    return render_template('lab4/sub-form.html')
+
+
+@lab4.route('/lab4/sub', methods=['POST'])
+def sub():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    
+    if x1 == '' or x2 == '':
+        return render_template('lab4/sub.html', error='Оба поля должны быть заполнены!')
+    
+    try:
+        x1 = int(x1)
+        x2 = int(x2)
+        result = x1 - x2
+        return render_template('lab4/sub.html', x1=x1, x2=x2, result=result)
+    
+    except ValueError:
+        return render_template('lab4/sub.html', error='Пожалуйста, введите корректные числа!')
+
+
+@lab4.route('/lab4/pow-form')
+def pow_form():
+    return render_template('lab4/pow-form.html')
+
+
+@lab4.route('/lab4/pow', methods=['POST'])
+def pow():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    
+    if x1 == '' or x2 == '':
+        return render_template('lab4/pow.html', error='Оба поля должны быть заполнены!')
+    
+    try:
+        x1 = int(x1)
+        x2 = int(x2)
+        
+        if x1 == 0 and x2 == 0:
+            return render_template('lab4/pow.html', error='Недопустимые значения!')
+        
+        result = x1 ** x2
+        return render_template('lab4/pow.html', x1=x1, x2=x2, result=result)
+    
+    except ValueError:
+        return render_template('lab4/pow.html', error='Пожалуйста, введите корректные числа!')
 
