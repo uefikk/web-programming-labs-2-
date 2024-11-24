c49dc69f6b365096180cf391dc2d6d744dfe8466
Ефимова Юлия <uefimova81@gmail.com>
2024-11-24 23:11:29 +0700

    заготовка к 5 лабе

---
+++ lab5.py
@@ -0,0 +1,6 @@
+from flask import Blueprint, url_for, redirect, render_template, request, make_response
+lab5 = Blueprint('lab5', __name__)
+
+@lab5.route('/lab5/')
+def lab():
+    return render_template('lab5/index.html')
