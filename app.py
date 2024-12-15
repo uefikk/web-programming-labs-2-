99b10a0a9c4735147ab9b46ca4441f5e48c1524f
Ефимова Юлия <uefimova81@gmail.com>
2024-12-16 01:33:03 +0700

    заготовка к 7-ой лабораторной

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
+from lab7 import lab7
 
 app = Flask(__name__)
 
@@ -20,6 +21,7 @@ app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
+app.register_blueprint(lab7)
 
 @app.errorhandler(404)
 def not_found(err):
@@ -58,6 +60,7 @@ def index():
                 <a href='/lab4/'>Четвертая лабораторная</a>
                 <a href='/lab5/'>Пятая лабораторная</a>
                 <a href='/lab6/'>Шестая лабораторная</a>
+                <a href='/lab7/'>Седьмая лабораторная</a>
            </body>
            <footer>Ефимова Юлия Алексеевна, ФБИ-22, 3 курс, 2024</footer>
         </html>''', 200
