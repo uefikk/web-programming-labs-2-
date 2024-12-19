81cde1e2d2e36edf729842142062ba5626cabd88
Ефимова Юлия <uefimova81@gmail.com>
2024-12-20 01:50:08 +0700

    Регистрация

--- lab8.py
+++ lab8.py
@@ -2,9 +2,31 @@ from flask import Blueprint, render_template, abort, request, jsonify
 from random import randint
 import psycopg2
 from psycopg2.extras import RealDictCursor
+from db import db
+from db.models import users, articles
+from werkzeug.security import check_password_hash, generate_password_hash
 
 lab8 = Blueprint('lab8', __name__)
 
 @lab8.route('/lab8/')
 def main():
     return render_template('lab8/lab8.html')
+
+
+@lab8.route('/lab8/register/', methods= ['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('lab8/register.html')
+    
+    login_form = request.form.get('login')
+    password_form = request.form.get('password')
+    
+    login_exists = users.query.filter_by(login=login_form).first()
+    if login_exists:
+        return render_template('lab8/register.html',
+                            error = '123123123')
+    
+    password_hash = generate_password_hash(password_form)
+    new_user = users(login = login_form, password = password_hash)
+    db.session.add(new_user)
+    db.session.commit()
